efd5be8c507a77132fd52959e7580263ff99a974
alexC317 <alex.f.cepeda@gmail.com>
2018-11-07 00:20:40 -0500

    Added the WindowMaster project to M1/Lessons and the framework for the first programming exercise to the folder.

---
+++ oop/M1/Lessons/WindowMaster/src/windowmaster/WindowMaster.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package windowmaster;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alex
+ */
+public class WindowMaster {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //decalre variables for height and width
+        float height;
+        float width;
+        // declare String variables to hold the user's height and 
+        // width input
+        String stringHeight;
+        String stringWidth;
+        
+        //declare other variabkes
+        float areaOfWindow;
+        float cost;
+        float perimeterOfWindow;
+        
+        //declare and initialize the Scanner
+        Scanner myScanner = new Scanner(System.in);
+        
+        // get input from the user
+        System.out.println("Please enter window height: ");
+        stringHeight = myScanner.nextLine();
+        System.out.println("Please enter window width");
+        stringWidth = myScanner.nextLine();
+        
+        // convert String values of height and width to float values
+        height = Float.parseFloat(stringHeight);
+        width = Float.parseFloat(stringWidth);
+        
+        // calculate the area of the window
+        areaOfWindow = height * width;
+        
+        // calculate the perimeter of the window
+        perimeterOfWindow = 2*(height + width);
+        
+        cost = ((3.50f * areaOfWindow) + (2.25f * perimeterOfWindow));
+        
+        System.out.println("Window height = " + stringHeight);
+        System.out.println("Window width = " + stringWidth);
+        System.out.println("Window area = " + areaOfWindow);
+        System.out.println("Window perimeter = " + perimeterOfWindow);
+        System.out.println("Total Cost = "+ cost);
+    }
+    
+}
