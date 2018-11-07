607d37558367406d1310828a575ec7dc811e0e35
alexC317 <alex.f.cepeda@gmail.com>
2018-11-07 02:30:21 -0500

    Added examples from the second programming exercise.

---
+++ oop/M1/Exercises/programmingExercise2/InABucket/src/inabucket/InABucket.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inabucket;
+
+/**
+ *
+ * @author Alex
+ */
+public class InABucket {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //You can declare all KINDS of variables
+        String walrus;
+        double piesEaten;
+        float weightofTeacupPig;
+        int grainsofSand;
+        
+        // But declaring them just sets up the space for data 
+        // to use the variable, you have to put data IN it first!
+        walrus = "Sir Leroy Jenkins III";
+        piesEaten = 42.1;
+        
+        System.out.println("Meet my pet walrus, " + walrus);
+        System.out.print("He was a bit hungry today, and ate this many pies : ");
+        System.out.println(piesEaten);
+    }
+    
+}
---
+++ oop/M1/Exercises/programmingExercise2/TheOrderOfThings/src/theorderofthings/TheOrderOfThings.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package theorderofthings;
+
+/**
+ *
+ * @author Alex
+ */
+public class TheOrderOfThings {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double number;
+        String opinion, size, age,shape, color, origin, material, purpose;
+        String noun;
+        
+        number = 5.0;
+        opinion = "AWESOME";
+        size = "teensy-weensy";
+        age = "new";
+        shape = "oblong";
+        color = "BRIGHT yellow";
+        origin = "AlphaCentaurian";
+        material = "platinum";
+        purpose = "good";
+        
+        noun = "dragons";
+        
+        // Using the + with strings, doesn't add it concatenates (sticks them together)
+        
+        System.out.println(number + opinion + size + age + shape + color + origin + material + purpose + noun);
+        
+    }
+    
+}
