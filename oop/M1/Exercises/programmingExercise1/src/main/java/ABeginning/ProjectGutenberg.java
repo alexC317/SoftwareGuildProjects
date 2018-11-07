ce8b4ebbe244ee203b9a7484d6c81d226eccff5e
alexC317 <alex.f.cepeda@gmail.com>
2018-11-07 00:58:40 -0500

    Completed ABeginning.java, ProjectGutenberg.java and CommentingCode.java.

---
+++ oop/M1/Exercises/programmingExercise1/src/main/java/ABeginning/ProjectGutenberg.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ABeginning;
+
+/**
+ *
+ * @author Alex
+ */
+public class ProjectGutenberg {
+    public static void main(String[] args){
+        System.out.println("Did you know that in 1440 (or thereabouts)");
+        System.out.println("Johannes Gutenberg invented the printing press?");
+        System.out.println("He started out as a goldsmith!");
+        System.out.print("His invention made rapid and prolific typed volumes ");
+        System.out.println("available for the first time ever...!");
+        System.out.println("We are like a modern Gutenberg!");
+        System.out.println("Printing vast amounts to the waiting console with ease!");
+    }
+            
+}
