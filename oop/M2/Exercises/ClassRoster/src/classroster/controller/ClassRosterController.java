f74d9a3691fcfba550edd5584ee46d8b513486ce
Alex Cepeda <alex.f.cepeda@gmail.com>
2018-12-05 15:43:03 -0500

    Finished Class Roster Controller

--- oop/M2/Exercises/ClassRoster/src/classroster/controller/ClassRosterController.java
+++ oop/M2/Exercises/ClassRoster/src/classroster/controller/ClassRosterController.java
@@ -1,15 +1,54 @@
 package classroster.controller;
 
+import classroster.ui.UserIO;
+import classroster.ui.UserIOConsoleImpl;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Alex
  */
 public class ClassRosterController {
-    
+
+    private UserIO io = new UserIOConsoleImpl();
+
+    public void run() {
+        boolean keepGoing = true;
+        int menuSelection = 0;
+        while(keepGoing){
+            io.print("Main Menu");
+            io.print("1. List Student IDs");
+            io.print("2. Create New Student");
+            io.print("3. View a Student");
+            io.print("4. Remove a Student");
+            io.print("5. Exit");
+            
+            menuSelection = io.readInt("Please select from the above choice.", 1, 5);
+            
+            switch(menuSelection){
+                case 1:
+                    io.print("LIST STUDENTS");
+                    break;
+                case 2:
+                    io.print("CREATE STUDENT");
+                    break;
+                case 3:
+                    io.print("VIEW STUDENT");
+                    break;
+                case 4:
+                    io.print("REMOVE STUDENT");
+                    break;
+                case 5:
+                    keepGoing = false;
+                    break;
+                default:
+                    io.print("UNKNOWN COMMAND");
+            }
+        }
+        io.print("GOOD BYE");
+    }
 }
