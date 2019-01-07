16914b1947f7b3ea126352ef17a2beef8eef6ad3
alexC317 <alex.f.cepeda@gmail.com>
2019-01-06 21:36:09 -0500

    Finished unit tests for Service Layer and all functions for the Service Layer implemented.

--- oop/M3/Assignments/VendingMachine/src/vendingmachine/service/VendingMachineServiceImpl.java
+++ oop/M3/Assignments/VendingMachine/src/vendingmachine/service/VendingMachineServiceImpl.java
@@ -6,6 +6,7 @@
 package vendingmachine.service;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -50,7 +51,7 @@ public class VendingMachineServiceImpl implements VendingMachineService {
     public Change vend(int itemId) throws InsufficientFundsException {
         // Search the Dao for the item specified (using its itemId)
         // Get the price of that item
-        // If the balance > price, subtract 1 from the inventory of that item,
+        // If the balance >= price, subtract 1 from the inventory of that item,
         // calculate change, and set balance to 0
         // If balance < price, throw an exception
         VendingMachineItem item = dao.readByID(itemId);
@@ -65,15 +66,44 @@ public class VendingMachineServiceImpl implements VendingMachineService {
             throw new InsufficientFundsException("Not enough funds.");
         }
         balance = null;
-        
+
         return change;
     }
 
     @Override
-    public Change calculateChange(BigDecimal change) {
+    public Change calculateChange(BigDecimal changeToCalculate) {
         // Take the change (in pennies), start with the biggest denominations 
         // and start calcuating how many quarters/dimes/nickels/pennies they're
         // getting back. Save all those in a Change object.
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Change change = new Change();
+        BigDecimal quarterValue = new BigDecimal(".25");
+        BigDecimal dimeValue = new BigDecimal(".10");
+        BigDecimal nickelValue = new BigDecimal(".05");
+        BigDecimal pennyValue = new BigDecimal(".01");
+
+        if (changeToCalculate.compareTo(quarterValue) != -1) {
+            int quarters = changeToCalculate.divide(quarterValue, RoundingMode.FLOOR).intValue();
+            change.setQuarters(quarters);
+            changeToCalculate = changeToCalculate.subtract(quarterValue.multiply(new BigDecimal(quarters)));
+        }
+
+        if (changeToCalculate.compareTo(dimeValue) != -1) {
+            int dimes = changeToCalculate.divide(dimeValue, RoundingMode.FLOOR).intValue();
+            change.setDimes(dimes);
+            changeToCalculate = changeToCalculate.subtract(dimeValue.multiply(new BigDecimal(dimes)));
+        }
+
+        if (changeToCalculate.compareTo(nickelValue) != -1) {
+            int nickels = changeToCalculate.divide(nickelValue, RoundingMode.FLOOR).intValue();
+            change.setNickels(nickels);
+            changeToCalculate = changeToCalculate.subtract(nickelValue.multiply(new BigDecimal(nickels)));
+        }
+
+        if (changeToCalculate.compareTo(pennyValue) != -1) {
+            int pennies = changeToCalculate.divide(pennyValue, RoundingMode.FLOOR).intValue();
+            change.setPennies(pennies);
+        }
+
+        return change;
     }
 }
--- oop/M3/Assignments/VendingMachine/test/vendingmachine/service/VendingMachineServiceTest.java
+++ oop/M3/Assignments/VendingMachine/test/vendingmachine/service/VendingMachineServiceTest.java
@@ -63,43 +63,111 @@ public class VendingMachineServiceTest {
     public void testSetAndGetBalance() {
         BigDecimal balance = new BigDecimal("1.00");
         service.setBalance(balance);
-        assertEquals(0, balance.compareTo(service.getBalance()));
+        assertEquals(balance, service.getBalance());
     }
 
     /**
-     * Test of vend method, of class VendingMachineService.
+     * Test of vend method, of class VendingMachineService, assuming the balance
+     * is equal to the price.
+     *
+     * @throws Exception
      */
     @Test
-    public void testVend() throws InsufficientFundsException {
-        BigDecimal balance = new BigDecimal("1.00");
-        service.setBalance(balance);
-        service.vend(1);
-        
+    public void testVendIfNoChange() throws Exception {
+        //Test to see if the inventory of the item was successfully vended
+        //Test to see if there was no change given
         List<VendingMachineItem> itemList = service.getAvailableItems();
-        assertEquals(1, itemList.get(0).getItemCount());
+        assertEquals(1, itemList.size());
+        BigDecimal balance = new BigDecimal("1.00");
+        Change change;
+        
+        service.setBalance(balance);
+        change = service.vend(1);
+
+        itemList = service.getAvailableItems();
+        assertEquals(0, itemList.size());
+        
+        assertEquals(0, change.getQuarters());
+        assertEquals(0, change.getDimes());
+        assertEquals(0, change.getNickels());
+        assertEquals(0, change.getPennies());
         assertNull(service.getBalance());
     }
-    
+
+    /**
+     * Test of vend method, of class VendingMachineService, assuming the balance
+     * is more than the price.
+     *
+     * @throws Exception
+     */
     @Test
-    public void testVendIfNoChange() {
+    public void testVendIfChange() throws Exception {
+        //Test to see if the inventory of the item was successfully vended
+        //Test to see if there was correct change given
+        BigDecimal balance = new BigDecimal("1.50");
+        Change change;
         
+        service.setBalance(balance);
+        change = service.vend(1);
+        
+        List<VendingMachineItem> itemList = service.getAvailableItems();
+        assertEquals(0, itemList.size());
+        
+        assertEquals(2, change.getQuarters());
+        assertEquals(0, change.getDimes());
+        assertEquals(0, change.getNickels());
+        assertEquals(0, change.getPennies());
+        assertNull(service.getBalance());
+
     }
-    
+
+    /**
+     * Test of vend method of class VendingMachineService, assuming the balance
+     * is less than the price.
+     *
+     * @throws Exception
+     */
     @Test
-    public void testVendIfChange() {
-        
+    public void testVendIfNoEnoughBalance() throws Exception {
+        //Test to see if the inventory of the item was not successfully vended
+        //Test to see if there was an Exception thrown
+
     }
-    
-    @Test
-    public void testVendIfNoEnoughBalance() {
-        
-    }
-    
+
     /**
      * Test of calculateChange method, of class VendingMachineService.
      */
     @Test
     public void testCalculateChange() {
+        BigDecimal dollar = new BigDecimal("1.00");
+        BigDecimal seventyCents = new BigDecimal(".70");
+        BigDecimal fourtyTwoCents = new BigDecimal (".42");
+        BigDecimal sevenCents = new BigDecimal (".07");
+        
+        Change change1 = service.calculateChange(dollar);
+        Change change2 = service.calculateChange(seventyCents);
+        Change change3 = service.calculateChange(fourtyTwoCents);
+        Change change4 = service.calculateChange(sevenCents);
+        
+        assertEquals(4, change1.getQuarters());
+        assertEquals(0, change1.getDimes());
+        assertEquals(0, change1.getNickels());
+        assertEquals(0, change1.getPennies());
+        
+        assertEquals(2, change2.getQuarters());
+        assertEquals(2, change2.getDimes());
+        assertEquals(0, change2.getNickels());
+        assertEquals(0, change2.getPennies());
+        
+        assertEquals(1, change3.getQuarters());
+        assertEquals(1, change3.getDimes());
+        assertEquals(1, change3.getNickels());
+        assertEquals(2, change3.getPennies());
+        
+        assertEquals(0, change4.getQuarters());
+        assertEquals(0, change4.getDimes());
+        assertEquals(1, change4.getNickels());
+        assertEquals(2, change4.getPennies());
     }
 
 }
