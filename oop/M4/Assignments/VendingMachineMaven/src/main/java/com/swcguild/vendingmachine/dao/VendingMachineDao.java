0545131471d0215bdbd97d819231abd8416c44b9
Alex Cepeda <alex.f.cepeda@gmail.com>
2019-01-23 19:21:32 -0500

    ALEXCEPEDAINDIVIDUALWORK-4 Created the VendingMachinePersistenceException class for the DAO package. Took the VendingMachineDao and VendingMachineDaoFileImpl code from the original project and moved it to the Maven project.

--- oop/M4/Assignments/VendingMachineMaven/src/main/java/com/swcguild/vendingmachine/dao/VendingMachineDao.java
+++ oop/M4/Assignments/VendingMachineMaven/src/main/java/com/swcguild/vendingmachine/dao/VendingMachineDao.java
@@ -5,10 +5,45 @@
  */
 package com.swcguild.vendingmachine.dao;
 
+import com.swcguild.vendingmachine.dto.VendingMachineItem;
+import java.util.List;
+
 /**
  *
  * @author Alex
  */
 public interface VendingMachineDao {
-    
+
+    /**
+     * Creates a new item to place into memory.
+     *
+     * @param item The item to be saved into memory.
+     * @throws VendingMachinePersistenceException
+     */
+    public void create(VendingMachineItem item) throws VendingMachinePersistenceException;
+
+    /**
+     * Returns a list of all VendingMachineItems saved in the DAO.
+     *
+     * @return A list of type VendingMachineItem/
+     */
+    public List<VendingMachineItem> readAll();
+
+    /**
+     * Returns an object saved in the HashMap, referenced by its id.
+     *
+     * @param itemId
+     * @return
+     */
+    public VendingMachineItem readByID(int itemId);
+
+    /**
+     * Updates an item using the VendingMachineItem passed into it as reference.
+     *
+     * @param itemId
+     * @param item
+     * @throws VendingMachinePersistenceException
+     */
+    public void update(int itemId, VendingMachineItem item) throws VendingMachinePersistenceException;
+
 }
--- oop/M4/Assignments/VendingMachineMaven/src/main/java/com/swcguild/vendingmachine/dao/VendingMachineDaoFileImpl.java
+++ oop/M4/Assignments/VendingMachineMaven/src/main/java/com/swcguild/vendingmachine/dao/VendingMachineDaoFileImpl.java
@@ -5,10 +5,127 @@
  */
 package com.swcguild.vendingmachine.dao;
 
+import com.swcguild.vendingmachine.dto.VendingMachineItem;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  *
  * @author Alex
  */
-public class VendingMachineDaoFileImpl {
-    
+public class VendingMachineDaoFileImpl implements VendingMachineDao {
+
+    private Map<Integer, VendingMachineItem> items = new HashMap<>();
+
+    public static final String INVENTORY_FILE = "inventory.txt";
+    public static final String DELIMITER = "::";
+
+    // LOAD THE FILE INTO MEMORY IN THE CONSTRUCTOR
+    public VendingMachineDaoFileImpl() throws VendingMachinePersistenceException {
+        loadInventory();
+    }
+
+    @Override
+    public void create(VendingMachineItem item) throws VendingMachinePersistenceException {
+        items.put(item.getItemId(), item);
+
+        writeInventory();
+    }
+
+    @Override
+    public List<VendingMachineItem> readAll() {
+        return new ArrayList<>(items.values());
+    }
+
+    @Override
+    public VendingMachineItem readByID(int itemId) {
+        if (items.containsKey(itemId)) {
+            return items.get(itemId);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void update(int itemId, VendingMachineItem item) throws VendingMachinePersistenceException {
+        if (items.containsKey(itemId)) {
+            if (items.get(itemId).getItemName().equals(item.getItemName())) {
+            } else {
+                items.get(itemId).setItemName(item.getItemName());
+            }
+
+            if (items.get(itemId).getItemCount() == item.getItemCount()) {
+            } else {
+                items.get(itemId).setItemCount(item.getItemCount());
+            }
+
+            if (items.get(itemId).getItemPrice().equals(item.getItemPrice())) {
+            } else {
+                items.get(itemId).setItemPrice(item.getItemPrice());
+            }
+        }
+
+        writeInventory();
+    }
+
+    private void loadInventory() throws VendingMachinePersistenceException {
+        Scanner scanner;
+
+        try {
+            //Create scanner for reading the file
+            scanner = new Scanner(
+                    new BufferedReader(
+                            new FileReader(INVENTORY_FILE)));
+        } catch (FileNotFoundException e) {
+            throw new VendingMachinePersistenceException(
+                    "-_- Could not load roster into memory.", e);
+        }
+
+        String currentLine;
+        String[] currentTokens;
+
+        while (scanner.hasNextLine()) {
+            currentLine = scanner.nextLine();
+            currentTokens = currentLine.split(DELIMITER);
+
+            VendingMachineItem currentItem = new VendingMachineItem(Integer.parseInt(currentTokens[0]));
+            currentItem.setItemName(currentTokens[1]);
+            currentItem.setItemPrice(new BigDecimal(currentTokens[2]));
+            currentItem.setItemCount(Integer.parseInt(currentTokens[3]));
+
+            items.put(currentItem.getItemId(), currentItem);
+        }
+        scanner.close();
+    }
+
+    private void writeInventory() throws VendingMachinePersistenceException {
+        PrintWriter out;
+
+        try {
+            out = new PrintWriter(new FileWriter(INVENTORY_FILE));
+        } catch (IOException e) {
+            throw new VendingMachinePersistenceException("Could not save inventory data.", e);
+        }
+
+        List<VendingMachineItem> itemList = this.readAll();
+        for (VendingMachineItem currentItem : itemList) {
+            out.println(currentItem.getItemId() + DELIMITER
+                    + currentItem.getItemName() + DELIMITER
+                    + currentItem.getItemPrice() + DELIMITER
+                    + currentItem.getItemCount());
+            out.flush();
+        }
+        out.close();
+    }
+
 }
