e4a66d8c5008507407502e84d9c02f9eae161754
Alex Cepeda <alex.f.cepeda@gmail.com>
2019-01-23 19:06:55 -0500

    ALEXCEPEDAINDIVIDUALWORK-4 Modified POM file to fix the Artifact ID and Display Name of the project. Copied and pasted the Change and VendingMachineItem classes from the original project to the Maven version.

--- oop/M4/Assignments/VendingMachineMaven/src/main/java/com/swcguild/vendingmachine/dto/Change.java
+++ oop/M4/Assignments/VendingMachineMaven/src/main/java/com/swcguild/vendingmachine/dto/Change.java
@@ -10,5 +10,41 @@ package com.swcguild.vendingmachine.dto;
  * @author Alex
  */
 public class Change {
-    
+
+    private int quarters;
+    private int dimes;
+    private int nickels;
+    private int pennies;
+
+    public int getQuarters() {
+        return quarters;
+    }
+
+    public void setQuarters(int quarters) {
+        this.quarters = quarters;
+    }
+
+    public int getDimes() {
+        return dimes;
+    }
+
+    public void setDimes(int dimes) {
+        this.dimes = dimes;
+    }
+
+    public int getNickels() {
+        return nickels;
+    }
+
+    public void setNickels(int nickels) {
+        this.nickels = nickels;
+    }
+
+    public int getPennies() {
+        return pennies;
+    }
+
+    public void setPennies(int pennies) {
+        this.pennies = pennies;
+    }
 }
--- oop/M4/Assignments/VendingMachineMaven/src/main/java/com/swcguild/vendingmachine/dto/VendingMachineItem.java
+++ oop/M4/Assignments/VendingMachineMaven/src/main/java/com/swcguild/vendingmachine/dto/VendingMachineItem.java
@@ -5,10 +5,48 @@
  */
 package com.swcguild.vendingmachine.dto;
 
+import java.math.BigDecimal;
+
 /**
  *
  * @author Alex
  */
 public class VendingMachineItem {
-    
+
+    private int itemId;
+    private String itemName;
+    private BigDecimal itemPrice;
+    private int itemCount;
+
+    public VendingMachineItem(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public BigDecimal getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(BigDecimal itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public int getItemCount() {
+        return itemCount;
+    }
+
+    public void setItemCount(int itemCount) {
+        this.itemCount = itemCount;
+    }
 }
