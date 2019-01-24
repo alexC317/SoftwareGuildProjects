a6fb96e066fbfbd9e89996278fa346b8cfa21e0c
Alex Cepeda <alex.f.cepeda@gmail.com>
2019-01-23 20:44:50 -0500

    ALEXCEPEDAINDIVIDUALWORK-4 Created the Service Layer for VendingMachineMaven. Filled out the Service Layer interface and its implementation and created all the Exception classes.

--- oop/M4/Assignments/VendingMachineMaven/src/main/java/com/swcguild/vendingmachine/service/VendingMachineServiceLayer.java
+++ oop/M4/Assignments/VendingMachineMaven/src/main/java/com/swcguild/vendingmachine/service/VendingMachineServiceLayer.java
@@ -5,10 +5,84 @@
  */
 package com.swcguild.vendingmachine.service;
 
+import com.swcguild.vendingmachine.dao.VendingMachinePersistenceException;
+import com.swcguild.vendingmachine.dto.Change;
+import com.swcguild.vendingmachine.dto.VendingMachineItem;
+import java.math.BigDecimal;
+import java.util.List;
+
 /**
  *
  * @author Alex
  */
 public interface VendingMachineServiceLayer {
-    
+
+    /**
+     * Adds a new item to the Vending Machine. Should only be accessible through
+     * the secret menu.
+     *
+     * @param item
+     * @throws vendingmachine.service.VendingMachineDuplicateIDException
+     * @throws vendingmachine.dao.VendingMachinePersistenceException
+     * @throws vendingmachine.service.VendingMachineDataValidationException
+     */
+    public void addNewItem(VendingMachineItem item) throws
+            VendingMachineDuplicateIDException,
+            VendingMachinePersistenceException,
+            VendingMachineDataValidationException;
+
+    /**
+     * Get a list of all items from the Dao layer.
+     *
+     * @return A list of items that do not have an inventory of 0.
+     */
+    public List<VendingMachineItem> getAvailableItems();
+
+    /**
+     * Takes the balance retrieved from the Controller and stores it in a
+     * variable.
+     *
+     * @param balance The balance retrieved from the Controller.
+     */
+    public void setBalance(BigDecimal balance);
+
+    /**
+     * Takes the balance stored in the Service Layer and returns it to the
+     * caller.
+     *
+     * @return The balance stored in the Service Layer.
+     */
+    public BigDecimal getBalance();
+
+    /**
+     * Do all the necessary functions related to vending an item.
+     *
+     * @param itemId
+     * @return The change being returned back to the user.
+     * @throws vendingmachine.service.InsufficientFundsException
+     * @throws vendingmachine.dao.VendingMachinePersistenceException
+     * @throws vendingmachine.service.ItemOutOfStockException
+     */
+    public Change vend(int itemId) throws InsufficientFundsException, VendingMachinePersistenceException, ItemOutOfStockException;
+
+    /**
+     * Calculates how much of each denomination the User is getting back and
+     * returns the amount to the Controller.
+     *
+     * @param change The difference between the balance and the cost of the
+     * item.
+     * @return A Change object containing how much of each denomination the user
+     * is getting back.
+     */
+    public Change calculateChange(BigDecimal change);
+
+    /**
+     *
+     * @param itemId
+     * @param resupplyAmount
+     * @throws VendingMachinePersistenceException
+     * @throws VendingMachineItemOverCapacityException
+     */
+    public void restock(int itemId, int resupplyAmount) throws VendingMachinePersistenceException, VendingMachineItemOverCapacityException;
+
 }
--- oop/M4/Assignments/VendingMachineMaven/src/main/java/com/swcguild/vendingmachine/service/VendingMachineServiceLayerImpl.java
+++ oop/M4/Assignments/VendingMachineMaven/src/main/java/com/swcguild/vendingmachine/service/VendingMachineServiceLayerImpl.java
@@ -5,10 +5,137 @@
  */
 package com.swcguild.vendingmachine.service;
 
+import com.swcguild.vendingmachine.dao.VendingMachineAuditDao;
+import com.swcguild.vendingmachine.dao.VendingMachineDao;
+import com.swcguild.vendingmachine.dao.VendingMachinePersistenceException;
+import com.swcguild.vendingmachine.dto.Change;
+import com.swcguild.vendingmachine.dto.VendingMachineItem;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
+
 /**
  *
  * @author Alex
  */
-public class VendingMachineServiceLayerImpl {
-    
+public class VendingMachineServiceLayerImpl implements VendingMachineServiceLayer {
+
+    private BigDecimal balance;
+    private VendingMachineDao dao;
+    private VendingMachineAuditDao auditDao;
+
+    public VendingMachineServiceLayerImpl(VendingMachineDao dao, VendingMachineAuditDao auditDao) throws VendingMachinePersistenceException {
+        this.dao = dao;
+        this.auditDao = auditDao;
+    }
+
+    @Override
+    public void addNewItem(VendingMachineItem item) throws
+            VendingMachineDuplicateIDException,
+            VendingMachinePersistenceException,
+            VendingMachineDataValidationException {
+        if (dao.readByID(item.getItemId()) != null) {
+            auditDao.writeAuditEntry("ID already in use.");
+            throw new VendingMachineDuplicateIDException("This ID is already in use. Please select another and try again.");
+        }
+        validateItem(item);
+        dao.create(item);
+        auditDao.writeAuditEntry(item.getItemName() + " added to the Vending Machine.");
+    }
+
+    @Override
+    public List<VendingMachineItem> getAvailableItems() {
+        return dao.readAll();
+    }
+
+    @Override
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance.setScale(2, RoundingMode.HALF_UP);
+    }
+
+    @Override
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    @Override
+    public Change vend(int itemId) throws InsufficientFundsException, VendingMachinePersistenceException, ItemOutOfStockException {
+        VendingMachineItem item = dao.readByID(itemId);
+        BigDecimal price = item.getItemPrice();
+        Change change;
+
+        if (item.getItemCount() == 0) {
+            String message = "Item not in stock.";
+            auditDao.writeAuditEntry(message);
+            throw new ItemOutOfStockException(message);
+        }
+        if (balance.compareTo(price) != -1) {
+            item.setItemCount(item.getItemCount() - 1);
+            dao.update(itemId, item);
+            change = calculateChange(balance.subtract(price));
+        } else {
+            String message = "Not enough funds.";
+            auditDao.writeAuditEntry(message);
+            throw new InsufficientFundsException(message);
+        }
+        balance = null;
+
+        return change;
+    }
+
+    @Override
+    public Change calculateChange(BigDecimal changeToCalculate) {
+        Change change = new Change();
+        BigDecimal quarterValue = new BigDecimal(".25");
+        BigDecimal dimeValue = new BigDecimal(".10");
+        BigDecimal nickelValue = new BigDecimal(".05");
+        BigDecimal pennyValue = new BigDecimal(".01");
+
+        if (changeToCalculate.compareTo(quarterValue) != -1) {
+            int quarters = changeToCalculate.divide(quarterValue, RoundingMode.FLOOR).intValue();
+            change.setQuarters(quarters);
+            changeToCalculate = changeToCalculate.subtract(quarterValue.multiply(new BigDecimal(quarters)));
+        }
+
+        if (changeToCalculate.compareTo(dimeValue) != -1) {
+            int dimes = changeToCalculate.divide(dimeValue, RoundingMode.FLOOR).intValue();
+            change.setDimes(dimes);
+            changeToCalculate = changeToCalculate.subtract(dimeValue.multiply(new BigDecimal(dimes)));
+        }
+
+        if (changeToCalculate.compareTo(nickelValue) != -1) {
+            int nickels = changeToCalculate.divide(nickelValue, RoundingMode.FLOOR).intValue();
+            change.setNickels(nickels);
+            changeToCalculate = changeToCalculate.subtract(nickelValue.multiply(new BigDecimal(nickels)));
+        }
+
+        if (changeToCalculate.compareTo(pennyValue) != -1) {
+            int pennies = changeToCalculate.divide(pennyValue, RoundingMode.FLOOR).intValue();
+            change.setPennies(pennies);
+        }
+
+        return change;
+    }
+
+    private void validateItem(VendingMachineItem item) throws VendingMachineDataValidationException {
+        if (item.getItemName() == null
+                || item.getItemName().trim().length() == 0
+                || item.getItemCount() <= 0
+                || item.getItemPrice() == null
+                || item.getItemPrice().compareTo(new BigDecimal("0.00")) != 1) {
+            throw new VendingMachineDataValidationException("ERROR: All fields [Item Name, Item Count, Item Price] must be valid.");
+        }
+    }
+
+    @Override
+    public void restock(int itemId, int resupplyAmount) throws VendingMachinePersistenceException, VendingMachineItemOverCapacityException {
+        VendingMachineItem item = dao.readByID(itemId);
+        int currentCount = item.getItemCount();
+        if (resupplyAmount + currentCount > 10) {
+            throw new VendingMachineItemOverCapacityException("More than 10 of this item in the machine.");
+        }
+        item.setItemCount(resupplyAmount + currentCount);
+        dao.update(itemId, item);
+    }
+
 }
