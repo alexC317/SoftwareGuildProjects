8d09f75c23e3b21164c5c5bb0a4fdf9a9c5180f4
alexC317 <alex.f.cepeda@gmail.com>
2019-02-19 06:57:01 -0500

    ALEXCEPEDAINDIVIDUALWORK-5 Program is now functional with the use of Spring dependency injection.

--- oop/Mastery/Flooring/src/main/java/com/swcguild/flooring/App.java
+++ oop/Mastery/Flooring/src/main/java/com/swcguild/flooring/App.java
@@ -9,15 +9,25 @@ import com.swcguild.flooring.controller.Controller;
 import com.swcguild.flooring.dao.FlooringPersistenceException;
 import com.swcguild.flooring.dao.OrderDAO;
 import com.swcguild.flooring.dao.OrderDAOProdFileImpl;
+import static com.swcguild.flooring.dao.OrderDAOProdFileImpl.DELIMITER;
+import com.swcguild.flooring.dao.OrderDAOTrainingFileImpl;
 import com.swcguild.flooring.dao.ProductDAO;
 import com.swcguild.flooring.dao.ProductDAOFileImpl;
 import com.swcguild.flooring.dao.TaxDAO;
 import com.swcguild.flooring.dao.TaxDAOFileImpl;
+import com.swcguild.flooring.dto.Order;
 import com.swcguild.flooring.service.Service;
 import com.swcguild.flooring.service.ServiceImpl;
 import com.swcguild.flooring.view.UserIO;
 import com.swcguild.flooring.view.UserIOConsoleImpl;
 import com.swcguild.flooring.view.View;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.math.BigDecimal;
+import java.util.Scanner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  *
@@ -26,14 +36,15 @@ import com.swcguild.flooring.view.View;
 public class App {
 
     public static void main(String[] args) throws FlooringPersistenceException {
-        UserIO IO = new UserIOConsoleImpl();
-        View view = new View(IO);
-        OrderDAO orderDAO = new OrderDAOProdFileImpl();
-        ProductDAO productDAO = new ProductDAOFileImpl();
-        TaxDAO taxDAO = new TaxDAOFileImpl();
-        Service service = new ServiceImpl(taxDAO, productDAO, orderDAO);
-        Controller controller = new Controller(view, service);
-
+//        UserIO IO = new UserIOConsoleImpl();
+//        View view = new View(IO);
+//        OrderDAO orderDAO = new OrderDAOProdFileImpl();
+//        ProductDAO productDAO = new ProductDAOFileImpl();
+//        TaxDAO taxDAO = new TaxDAOFileImpl();
+//        Service service = new ServiceImpl(taxDAO, productDAO, orderDAO);
+//        Controller controller = new Controller(view, service);
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Controller controller = ctx.getBean("controller", Controller.class);
         controller.run();
     }
 }
