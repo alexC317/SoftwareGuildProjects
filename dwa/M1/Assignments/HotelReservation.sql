262c096ab3e8548df3b97b624bf2e29156154dc0
alexC317 <alex.f.cepeda@gmail.com>
2019-03-06 23:33:25 -0500

    Updated and finished 1st draft of the script for HotelReservation.sql

--- dwa/M1/Assignments/HotelReservation.sql
+++ dwa/M1/Assignments/HotelReservation.sql
@@ -50,3 +50,47 @@ CREATE TABLE RoomAmenities(
     FOREIGN KEY (AmenityID) REFERENCES Amenities(AmenityID)
 );
 
+CREATE TABLE AddOns(
+	AddOnID INT PRIMARY KEY AUTO_INCREMENT,
+    AddOnName VARCHAR(50),
+    AddOnFee DECIMAL(8,2),
+    StartDate DATE NOT NULL,
+    EndDate DATE NOT NULL
+);
+
+CREATE TABLE PromotionalCodes(
+	PromotionalID INT PRIMARY KEY AUTO_INCREMENT,
+    StartDate DATE,
+    EndDate DATE,
+    Percent INT,
+    DollarAmt INT,
+    ReservationID INT NOT NULL,
+    FOREIGN KEY (ReservationID) REFERENCES Reservations(ReservationID)
+);
+
+CREATE TABLE Customers(
+	CustomerID INT PRIMARY KEY AUTO_INCREMENT,
+    CustomerName VARCHAR(50) NOT NULL,
+    CustomerPhone VARCHAR(10) NOT NULL,
+    CustomerEmail VARCHAR(50) NOT NULL,
+    CustomerAge INT NOT NULL,
+    ReservationID INT NOT NULL,
+    FOREIGN KEY (ReservationID) REFERENCES Reservations(ReservationID)
+);
+
+CREATE TABLE Billing(
+	BillingID INT PRIMARY KEY AUTO_INCREMENT,
+    Subtotal DECIMAL(8,2),
+    Tax DECIMAL(8,2),
+    Total DECIMAL(8,2),
+    ReservationID INT NOT NULL,
+    FOREIGN KEY (ReservationID) REFERENCES Reservations(ReservationID)
+);
+
+CREATE TABLE BillingDetails(
+	BillingDetailsID INT PRIMARY KEY AUTO_INCREMENT,
+    `Description` VARCHAR(50) NOT NULL,
+    Amount DECIMAL(8,2) NOT NULL,
+    BillingID INT NOT NULL,
+    FOREIGN KEY (BillingID) REFERENCES Billing(BillingID)
+);
