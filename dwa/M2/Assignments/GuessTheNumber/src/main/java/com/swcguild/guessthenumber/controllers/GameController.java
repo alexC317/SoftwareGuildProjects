7ce637530bca0e8a0bff493d949d01830d526723
alexC317 <alex.f.cepeda@gmail.com>
2019-03-31 21:12:36 -0400

    ALEXCEPEDAINDIVIDUALWORK-6 Minor visual changes to the POM file. Set correct database for application.properties. Created skeleton for the GameController class and some implementations.

--- dwa/M2/Assignments/GuessTheNumber/src/main/java/com/swcguild/guessthenumber/controllers/GameController.java
+++ dwa/M2/Assignments/GuessTheNumber/src/main/java/com/swcguild/guessthenumber/controllers/GameController.java
@@ -5,13 +5,75 @@
  */
 package com.swcguild.guessthenumber.controllers;
 
-import org.springframework.stereotype.Component;
+import com.swcguild.guessthenumber.dtos.Game;
+import com.swcguild.guessthenumber.dtos.Round;
+import com.swcguild.guessthenumber.services.GameService;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author Alex
  */
-@Component
+@RestController
+@RequestMapping("/api")
 public class GameController {
+    
+    private final GameService gameService;
+    
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+    
+    @PostMapping("/begin")
+    @ResponseStatus(HttpStatus.CREATED)
+    public int beginGame() {
+        Game game = gameService.beginGame();
+        return game.getID();
+    }
+    
+    @PostMapping("/guess")
+    public Round checkGuess(@RequestBody String guess, @RequestBody int gameID) {
+        Round round = gameService.checkGuess(guess, gameID);
+        return round;
+    }
+    
+    @GetMapping("/game")
+    public List<Game> getAllGames() {
+        List<Game> games = gameService.getAllGames();
+        for (Game game : games) {
+            if (!game.getIsFinished()) {
+                game.setAnswer("Still not solved!");
+            }
+        }
+        return games;
+    }
+    
+    @GetMapping("/game/{gameID}")
+    public ResponseEntity<Game> getGameByID(@PathVariable int gameID) {
+        Game game = gameService.getGameByID(gameID);
+        if (game == null) {
+            return new ResponseEntity(null, HttpStatus.NOT_FOUND);
+        }
+        
+        if (!game.getIsFinished()) {
+            game.setAnswer("Still not solved!");
+        }
+        return ResponseEntity.ok(game);
+    }
+    
+    @GetMapping("/rounds/{gameID}")
+    public List<Round> getAllRoundsByID(int gameID) {
+        List<Round> rounds = gameService.getAllRoundsByGameID(gameID);
+        return rounds;
+    }
     
 }
