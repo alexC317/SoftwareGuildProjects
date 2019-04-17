5b1244aa527956dbff86e6cc346dc2620186c478
alexC317 <alex.f.cepeda@gmail.com>
2019-04-16 22:25:40 -0400

    ALEXCEPEDAINDIVIDUALWORK-7 Modified home.html to have the balance id of the money buttons be their respective numerical value. Added functionality to onItemClick, onBalanceClick and onPurchaseClick events to write to the home.html file, as well as the capability to have a running total.

--- dwa/M3/Assignments/js/home.js
+++ dwa/M3/Assignments/js/home.js
@@ -1,18 +1,48 @@
+var runningTotal = 0;
+var ds = new DataService();
+
 function onItemClick(e) {
-    alert("This item has been clicked!");
+    e.preventDefault();
+
+    var item = $(this);
+    var itemID = item.data("itemid");
+    $("#purchaseItem").val(itemID);
 };
 
-function onPurchaseClick(e) {
-    alert("This button has been clicked!");
+function onBalanceClick(e) {
+    e.preventDefault();
+
+    var balance = $(this);
+    var balanceAmt = balance.data("balanceid");
+    runningTotal = runningTotal + balanceAmt;
+
+    $("#balanceInput").val((runningTotal / 100).toFixed(2));
 }
 
 function onPurchaseClick(e) {
-    alert("This button has been clicked!");
+    e.preventDefault();
+
+    //Ajax call here probably
+    runningTotal = 0;
+    $("#balanceInput").val(runningTotal.toFixed(2));
 }
+
+
+function onGetAllItemsSuccess(reponse) {
+
+}
+
+function logError(err) {
+    console.log(err);
+}
+
 
 $(document).ready(function () {
     //alert("All wired up!");
+
+    ds.getAllItems(onGetAllItemsSuccess);
+
     $(document).on("click", ".vendingItem", onItemClick);
-    $(document).on("click", ".balanceButton", onPurchaseClick);
-    $(document).on("click", ".purchaseButton", onPurchaseClick)
+    $(document).on("click", ".balanceButton", onBalanceClick);
+    $(document).on("click", ".purchaseButton", onPurchaseClick);
 });
