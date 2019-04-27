4f689d2c0ff02085fa7d21afcab88d91f545bdff
alexC317 <alex.f.cepeda@gmail.com>
2019-04-26 22:16:37 -0400

    ALEXCEPEDAINDIVIDUALWORK-14 Finished the mySQL script for the database and the test one.

---
+++ dwa/M4/Assignments/CreateSuperSightingDBScript.sql
@@ -0,0 +1,58 @@
+DROP DATABASE IF EXISTS superSightingDB;
+CREATE DATABASE superSightingDB;
+USE superSightingDB;
+
+CREATE TABLE Supers(
+	superID INT PRIMARY KEY AUTO_INCREMENT,
+    superName VARCHAR(50) NOT NULL UNIQUE,
+    superDescription VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Powers(
+	powerID INT PRIMARY KEY AUTO_INCREMENT,
+    powerName VARCHAR(50) NOT NULL,
+    powerDescription VARCHAR(50) NOT NULL
+);
+CREATE TABLE Locations(
+	locationID INT PRIMARY KEY AUTO_INCREMENT,
+    locationDescription VARCHAR(60) NOT NULL,
+    locationAddress VARCHAR(60) NOT NULL,
+    locationLongitude VARCHAR(50), 
+    locationLatitude VARCHAR(50)
+);
+
+CREATE TABLE Organizations(
+	organizationID INT PRIMARY KEY AUTO_INCREMENT, 
+    organizationName VARCHAR(50) NOT NULL,
+    organizatinDescription VARCHAR(50) NOT NULL,
+    organizationAddress VARCHAR(50) NOT NULL,
+    organizationContact VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Superpowers(
+	superID INT NOT NULL,
+    powerID INT NOT NULL,
+    FOREIGN KEY (superID) REFERENCES Supers(superID),
+    FOREIGN KEY (powerID) REFERENCES Powers(powerID),
+    CONSTRAINT PK_Superpower PRIMARY KEY (superID, powerID)
+);
+
+CREATE TABLE Sightings(
+	sightingID INT PRIMARY KEY AUTO_INCREMENT,
+    sightingDate DATE NOT NULL,
+    superID INT NOT NULL,
+    locationID INT NOT NULL,
+    FOREIGN KEY (superID) REFERENCES Supers(superID),
+    FOREIGN KEY (locationID) REFERENCES Locations(locationID)
+);
+
+CREATE TABLE Supers_Organzations(
+	superID INT NOT NULL,
+    organzationID INT NOT NULL,
+    FOREIGN KEY (superID) REFERENCES Supers(superID),
+    FOREIGN KEY (organzationID) REFERENCES Organizations(organizationID),
+    CONSTRAINT PK_SuperOrganization PRIMARY KEY (superID, organzationID)
+);
+
+
+
