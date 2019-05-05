fea37551ae60d873d82ca2c5c45989105ea3f8dc
Alex Cepeda <alex.f.cepeda@gmail.com>
2019-05-05 00:06:08 -0400

    ALEXCEPEDAINDIVIDUALWORK-27 Finished TDD for the Delete method, test passed.

--- dwa/M4/Assignments/supersighting/src/test/java/com/sg/supersighting/daos/SuperDAOJDBCImplTest.java
+++ dwa/M4/Assignments/supersighting/src/test/java/com/sg/supersighting/daos/SuperDAOJDBCImplTest.java
@@ -103,7 +103,7 @@ public class SuperDAOJDBCImplTest {
         s2.setSuperName("Womnder Woman");
         s2.setSuperDescription("The Amazon");
         List<Power> wonderWomanPower = new ArrayList<>();
-        
+
         wonderWomanPower.add(power);
 
         s.setSuperPowers(supermanPowers);
@@ -118,40 +118,77 @@ public class SuperDAOJDBCImplTest {
         assertTrue(fromDAO.contains(s2));
 
     }
-    
+
     @Test
-    public void testUpdateSuper(){
+    public void testUpdateSuper() {
         Super s = new Super();
         s.setSuperName("Superman");
         s.setSuperDescription("The greatest hero");
         List<Power> supermanPowers = new ArrayList<>();
-        
+
         Power power = new Power();
         power.setPowerName("Flight");
         power.setPowerDescription("Able to fly");
         powerDAO.addNewPower(power);
-        
+
         supermanPowers.add(power);
         s.setSuperPowers(supermanPowers);
-        
+
         superDAO.addNewSuper(s);
-        
+
         Super fromDAO = superDAO.getSuperByID(s.getSuperID());
         assertEquals(fromDAO, s);
-        
+
         s.setSuperDescription("The most human despite not being one");
         Power power2 = new Power();
         power2.setPowerName("Ice breath");
         power2.setPowerDescription("Able to freeze with breath");
         powerDAO.addNewPower(power2);
-        
+
         supermanPowers.add(power2);
         s.setSuperPowers(supermanPowers);
         superDAO.updateSuper(s);
         assertNotEquals(fromDAO, s);
-        
+
         fromDAO = superDAO.getSuperByID(s.getSuperID());
         assertEquals(fromDAO, s);
+
+    }
+
+    @Test
+    public void testDeleteSuper() {
+        Super s = new Super();
+        s.setSuperName("Superman");
+        s.setSuperDescription("The greatest superhero");
+        List<Power> supermanPowers = new ArrayList<>();
+
+        Power power = new Power();
+        power.setPowerName("Flight");
+        power.setPowerDescription("Able to fly");
+        powerDAO.addNewPower(power);
+
+        supermanPowers.add(power);
+        s.setSuperPowers(supermanPowers);
+        superDAO.addNewSuper(s);
+
+        Super s2 = new Super();
+        s2.setSuperName("Wonder Woman");
+        s2.setSuperDescription("The Amazon");
+        List<Power> wonderWomanPowers = new ArrayList<>();
+        wonderWomanPowers.add(power);
+        s2.setSuperPowers(wonderWomanPowers);
+        superDAO.addNewSuper(s2);
+
+        List<Super> supers = superDAO.getAllSupers();
+        assertEquals(2, supers.size());
+        assertTrue(supers.contains(s));
+        assertTrue(supers.contains(s2));
+
+        superDAO.deleteSuper(s.getSuperID());
+        supers = superDAO.getAllSupers();
+        assertEquals(1, supers.size());
+        assertTrue(supers.contains(s2));
+        assertFalse(supers.contains(s));
     }
 
 //    /**
