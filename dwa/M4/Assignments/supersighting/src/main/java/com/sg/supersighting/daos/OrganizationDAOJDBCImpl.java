1ea4c0348ee1d679d917587917e48b27adfb9134
alexC317 <alex.f.cepeda@gmail.com>
2019-05-08 22:11:16 -0400

    ALEXCEPEDAINDIVIDUALWORK-28 Had to edit the getLocationForOrganization to return null if an empty ResultSet was produced.

--- dwa/M4/Assignments/supersighting/src/main/java/com/sg/supersighting/daos/OrganizationDAOJDBCImpl.java
+++ dwa/M4/Assignments/supersighting/src/main/java/com/sg/supersighting/daos/OrganizationDAOJDBCImpl.java
@@ -16,6 +16,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
@@ -117,9 +118,13 @@ public class OrganizationDAOJDBCImpl implements OrganizationDAO {
     }
 
     private void getLocationForOrganization(Organization organization) {
-        Location location = jdbc.queryForObject(
-                SELECT_LOCATION_FOR_ORGANIZATION, new LocationMapper(), organization.getOrganizationID());
-        organization.setOrganizationAddress(location);
+        try {
+            Location location = jdbc.queryForObject(
+                    SELECT_LOCATION_FOR_ORGANIZATION, new LocationMapper(), organization.getOrganizationID());
+            organization.setOrganizationAddress(location);
+        } catch (EmptyResultDataAccessException ex) {
+            organization.setOrganizationAddress(null);
+        }
     }
 
     private void getSupersForOrganization(Organization organization) {
