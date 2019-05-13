0f1a3c18247ac17b1e22b8423e59ceb481e92eb4
alexC317 <alex.f.cepeda@gmail.com>
2019-05-13 03:19:20 -0400

    ALEXCEPEDAINDIVIDUALWORK-61 Did all functionality for the Sightings page.

---
+++ dwa/M4/Assignments/supersighting/src/main/java/com/sg/supersighting/controllers/SightingsController.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.supersighting.controllers;
+
+import com.sg.supersighting.daos.LocationDAO;
+import com.sg.supersighting.daos.SightingDAO;
+import com.sg.supersighting.daos.SuperDAO;
+import com.sg.supersighting.dtos.Location;
+import com.sg.supersighting.dtos.Sighting;
+import com.sg.supersighting.dtos.Super;
+import java.time.LocalDate;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ *
+ * @author Alex
+ */
+@Controller
+public class SightingsController {
+    
+    @Autowired
+    SightingDAO sightingDAO;
+    
+    @Autowired
+    SuperDAO superDAO;
+    
+    @Autowired
+    LocationDAO locationDAO;
+    
+    @PostMapping("addSighting")
+    public String addSighting(HttpServletRequest request) {
+        Sighting sighting = new Sighting();
+        String sightingDate = request.getParameter("sightingDate");
+        String superID = request.getParameter("superID");
+        String locationID = request.getParameter("locationID");
+        sighting.setSightingDate(LocalDate.parse(sightingDate));
+        sighting.setSightingSuper(superDAO.getSuperByID(Integer.parseInt(superID)));
+        sighting.setSightingLocation(locationDAO.getLocationByID(Integer.parseInt(locationID)));
+        sightingDAO.addNewSighting(sighting);
+        return "redirect:/Sightings";
+    }
+    
+    @GetMapping("Sightings")
+    public String displaySightings(Model model) {
+        List<Sighting> sightings = sightingDAO.getAllSightings();
+        List<Super> supers = superDAO.getAllSupers();
+        List<Location> locations = locationDAO.getAllLocations();
+        model.addAttribute("sightings", sightings);
+        model.addAttribute("supers", supers);
+        model.addAttribute("locations", locations);
+        return "Sightings";
+    }
+    
+    @GetMapping("editSighting")
+    public String editSighting(Integer sightingID, Model model) {
+        Sighting sighting = sightingDAO.getSightingByID(sightingID);
+        List<Super> supers = superDAO.getAllSupers();
+        List<Location> locations = locationDAO.getAllLocations();
+        model.addAttribute("sighting", sighting);
+        model.addAttribute("supers", supers);
+        model.addAttribute("locations", locations);
+        return "editSighting";
+    }
+    
+    @PostMapping("editSighting")
+    public String performEditSighting(HttpServletRequest request) {
+        Sighting sighting = new Sighting();
+        String sightingID = request.getParameter("sightingID");
+        String sightingDate = request.getParameter("sightingDate");
+        String superID = request.getParameter("superID");
+        String locationID = request.getParameter("locationID");
+        sighting.setSightingID(Integer.parseInt(sightingID));
+        sighting.setSightingDate(LocalDate.parse(sightingDate));
+        sighting.setSightingSuper(superDAO.getSuperByID(Integer.parseInt(superID)));
+        sighting.setSightingLocation(locationDAO.getLocationByID(Integer.parseInt(locationID)));
+        sightingDAO.updateSighting(sighting);
+        return "redirect:/Sightings";
+    }
+    
+    @GetMapping("deleteSighting")
+    public String deleteSighting(Integer sightingID) {
+        sightingDAO.deleteSighting(sightingID);
+        return "redirect:/Sightings";
+    }
+}
