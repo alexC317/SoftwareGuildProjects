0f5b07c07799048476128df9afcedcf9ffe7eb5a
alexC317 <alex.f.cepeda@gmail.com>
2019-05-20 17:33:29 -0400

    ALEXCEPEDAINDIVIDUALWORK-63 ALEXCEPEDAINDIVIDUALWORK-65 ALEXCEPEDAINDIVIDUALWORK-67 Added CRUD functions for Role and User interfaces.

--- dwa/Mastery/Assignments/supersightingsecurity/src/main/java/com/sg/supersighting/dao/RoleDAO.java
+++ dwa/Mastery/Assignments/supersightingsecurity/src/main/java/com/sg/supersighting/dao/RoleDAO.java
@@ -5,10 +5,28 @@
  */
 package com.sg.supersighting.dao;
 
+import com.sg.supersighting.dto.Role;
+import java.util.List;
+
 /**
  *
  * @author Alex
  */
 public interface RoleDAO {
-    
+
+    //Create
+    public Role create(Role role);
+
+    //Read All
+    public List<Role> readAll();
+
+    //Read By ID
+    public Role readByID(int roleID);
+
+    //Update
+    public boolean update(Role role);
+
+    //Delete
+    public boolean delete(int roleID);
+
 }
--- dwa/Mastery/Assignments/supersightingsecurity/src/main/java/com/sg/supersighting/dao/UserDAO.java
+++ dwa/Mastery/Assignments/supersightingsecurity/src/main/java/com/sg/supersighting/dao/UserDAO.java
@@ -5,10 +5,27 @@
  */
 package com.sg.supersighting.dao;
 
+import com.sg.supersighting.dto.User;
+import java.util.List;
+
 /**
  *
  * @author Alex
  */
 public interface UserDAO {
-    
+
+    //Create
+    public User create(User user);
+
+    //Read All
+    public List<User> readAll();
+
+    //Read By ID
+    public User readByID(int userID);
+
+    //Update
+    public boolean update(User user);
+
+    //Delete
+    public boolean delete(int userID);
 }
