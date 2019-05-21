7dba038152fe65478d92d1b4d2fd1d771884ca54
alexC317 <alex.f.cepeda@gmail.com>
2019-05-21 18:22:57 -0400

    ALEXCEPEDAINDIVIDUALWORK-63 ALEXCEPEDAINDIVIDUALWORK-65 ALEXCEPEDAINDIVIDUALWORK-67 Read All for UserDAOJDBCImpl has been successfully tested and implemented.

--- dwa/Mastery/Assignments/supersightingsecurity/src/test/java/com/sg/supersighting/dao/UserDAOJDBCImplTest.java
+++ dwa/Mastery/Assignments/supersightingsecurity/src/test/java/com/sg/supersighting/dao/UserDAOJDBCImplTest.java
@@ -56,17 +56,43 @@ public class UserDAOJDBCImplTest {
     }
 
     @Test
-    public void testCreate() {
+    public void testCreateAndReadByID() {
         Set<Role> roles = new HashSet();
         User user = new User();
         user.setUsername("username");
         user.setPassword("password");
         user.setRoles(roles);
+        user.setEnabled(true);
         userDAO.create(user);
 
         User fromDAO = userDAO.readByID(user.getUserID());
         assertEquals(fromDAO, user);
 
+    }
+
+    @Test
+    public void testReadAll() {
+        Set<Role> roles1 = new HashSet();
+        User user1 = new User();
+        user1.setUsername("test 1");
+        user1.setPassword("password");
+        user1.setRoles(roles1);
+        user1.setEnabled(true);
+        userDAO.create(user1);
+
+        Set<Role> roles2 = new HashSet();
+        User user2 = new User();
+        user2.setUsername("test 2");
+        user2.setPassword("p@ssw0rd");
+        user2.setRoles(roles2);
+        user2.setEnabled(false);
+        userDAO.create(user2);
+
+        List<User> users = userDAO.readAll();
+        assertEquals(2, users.size());
+        assertTrue(users.contains(user1));
+        assertTrue(users.contains(user2));
+        
     }
 
 }
