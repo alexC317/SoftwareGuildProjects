051a3dc44e34707234b70578488f8cb9f2b8ccc6
alexC317 <alex.f.cepeda@gmail.com>
2019-05-22 18:20:42 -0400

    ALEXCEPEDAINDIVIDUALWORK-63 ALEXCEPEDAINDIVIDUALWORK-65 ALEXCEPEDAINDIVIDUALWORK-67 TDD for RoleDAO (Create, Read All, Read By ID, Update and Delete) have all been sucessfully implemented and tested.

--- dwa/Mastery/Assignments/supersightingsecurity/src/main/java/com/sg/supersighting/dao/RoleDAOJDBCImpl.java
+++ dwa/Mastery/Assignments/supersightingsecurity/src/main/java/com/sg/supersighting/dao/RoleDAOJDBCImpl.java
@@ -10,6 +10,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
@@ -19,12 +20,19 @@ public class RoleDAOJDBCImpl implements RoleDAO {
 
     @Autowired
     private JdbcTemplate jdbc;
-    
+
+    private final String INSERT_NEW_ROLE = "INSERT INTO roles(userRole) VALUES (?)";
     private final String SELECT_ALL_ROLES = "SELECT roleID, userRole FROM roles";
+    private final String SELECT_ROLE_BY_ID = "SELECT roleID, userRole from roles WHERE roleID = ?";
+    private final String UPDATE_ROLE = "UPDATE roles SET userRole = ? WHERE roleID = ?";
+    private final String DELETE_ROLE = "DELETE FROM roles WHERE roleID = ?";
 
     @Override
     public Role create(Role role) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        jdbc.update(INSERT_NEW_ROLE, role.getRole());
+        int newID = jdbc.queryForObject("SELECT LAST_INSERT_ID()", Integer.class);
+        role.setRoleID(newID);
+        return role;
     }
 
     @Override
@@ -34,17 +42,21 @@ public class RoleDAOJDBCImpl implements RoleDAO {
 
     @Override
     public Role readByID(int roleID) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            return jdbc.queryForObject(SELECT_ROLE_BY_ID, new RoleMapper(), roleID);
+        } catch (DataAccessException ex) {
+            return null;
+        }
     }
 
     @Override
     public boolean update(Role role) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return jdbc.update(UPDATE_ROLE, role.getRole(), role.getRoleID()) > 0;
     }
 
     @Override
     public boolean delete(int roleID) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return jdbc.update(DELETE_ROLE, roleID) > 0;
     }
 
     public final static class RoleMapper implements RowMapper<Role> {
