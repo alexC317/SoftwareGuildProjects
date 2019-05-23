cff5018fdd304b7d3fc4493c0a7b439a45e31cfc
alexC317 <alex.f.cepeda@gmail.com>
2019-05-23 00:19:36 -0400

    ALEXCEPEDAINDIVIDUALWORK-63 ALEXCEPEDAINDIVIDUALWORK-65 ALEXCEPEDAINDIVIDUALWORK-67 ALEXCEPEDAINDIVIDUALWORK-66 Commented out the in memory version of global configuration and replaced it a Database version. Also created a method to start the encoding process in BCrypt.

--- dwa/Mastery/Assignments/supersightingsecurity/src/main/java/com/sg/supersighting/config/SecurityConfig.java
+++ dwa/Mastery/Assignments/supersightingsecurity/src/main/java/com/sg/supersighting/config/SecurityConfig.java
@@ -6,11 +6,14 @@
 package com.sg.supersighting.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  *
@@ -21,34 +24,44 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    public void configureGlobalInMemory(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("sidekick").password("{noop}password").roles("SIDEKICK")
-                .and()
-                .withUser("admin").password("{noop}password").roles("ADMIN", "SIDEKICK");
-    }
+    UserDetailsService userDetails;
+
+//    @Autowired
+//    public void configureGlobalInMemory(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//                .inMemoryAuthentication()
+//                .withUser("sidekick").password("{noop}password").roles("SIDEKICK")
+//                .and()
+//                .withUser("admin").password("{noop}password").roles("ADMIN", "SIDEKICK");
+//    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                
                 .antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers("/", "/home").permitAll()
                 .antMatchers("/css/**", "/js/**", "/fonts/**").permitAll()
                 .anyRequest().hasRole("SIDEKICK")
-                
                 .and()
                 .formLogin()
                 .loginPage("/login")
                 .failureUrl("/login?login_error=1")
                 .permitAll()
-                
                 .and()
                 .logout()
                 .logoutSuccessUrl("/")
                 .permitAll();
+    }
+    
+    @Autowired
+    public void configureGlobalInDB(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetails).passwordEncoder(bCryptPasswordEncoder());
+    }
+    
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
