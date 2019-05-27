c5cf858024d1177741e650abbf9857874bba8f37
Alex Cepeda <alex.f.cepeda@gmail.com>
2019-05-27 18:42:05 -0400

    ALEXCEPEDAINDIVIDUALWORK-63 ALEXCEPEDAINDIVIDUALWORK-64 ALEXCEPEDAINDIVIDUALWORK-74 Renamed the ErrorController to SightingErrorController to implement the ErrorController interface that Spring provides. Added code to pull the error number and display it to an error.html page. Modified the index page to display a login button if the user has not logged in. This button is not visible if the user is logged in.

---
+++ dwa/Mastery/Assignments/supersightingsecurity/src/main/java/com/sg/supersighting/controller/SightingErrorController.java
@@ -0,0 +1,36 @@
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package com.sg.supersighting.controller;
+//
+//import javax.servlet.RequestDispatcher;
+//import javax.servlet.http.HttpServletRequest;
+//import org.springframework.boot.web.servlet.error.ErrorController;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+///**
+// *
+// * @author Alex
+// */
+//@Controller
+//public class SightingErrorController implements ErrorController {
+//
+//    @GetMapping("/error")
+//    public String displayErrorPage(Model model, HttpServletRequest request) {
+//        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+//
+//        if (status != null) {
+//            model.addAttribute("errorMessage", "Error number: " + status.toString());
+//        }
+//        return "error";
+//    }
+//
+//    @Override
+//    public String getErrorPath() {
+//        return "/error";
+//    }
+//}
