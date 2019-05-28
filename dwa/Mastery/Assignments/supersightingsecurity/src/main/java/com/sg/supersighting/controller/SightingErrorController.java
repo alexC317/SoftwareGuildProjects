630804376c834fc89b6a02180bbd610597e45509
alexC317 <alex.f.cepeda@gmail.com>
2019-05-28 17:53:29 -0400

    ALEXCEPEDAINDIVIDUALWORK-63 ALEXCEPEDAINDIVIDUALWORK-64 Modifed the SightingErrorController and error.html to display an error message if one was caught. Also modified editOrganization to have the Organization's info saved if the editor is a Sidekick.

--- dwa/Mastery/Assignments/supersightingsecurity/src/main/java/com/sg/supersighting/controller/SightingErrorController.java
+++ dwa/Mastery/Assignments/supersightingsecurity/src/main/java/com/sg/supersighting/controller/SightingErrorController.java
@@ -1,36 +1,38 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package com.sg.supersighting.controller;
-//
-//import javax.servlet.RequestDispatcher;
-//import javax.servlet.http.HttpServletRequest;
-//import org.springframework.boot.web.servlet.error.ErrorController;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-///**
-// *
-// * @author Alex
-// */
-//@Controller
-//public class SightingErrorController implements ErrorController {
-//
-//    @GetMapping("/error")
-//    public String displayErrorPage(Model model, HttpServletRequest request) {
-//        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-//
-//        if (status != null) {
-//            model.addAttribute("errorMessage", "Error number: " + status.toString());
-//        }
-//        return "error";
-//    }
-//
-//    @Override
-//    public String getErrorPath() {
-//        return "/error";
-//    }
-//}
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.supersighting.controller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author Alex
+ */
+@Controller
+public class SightingErrorController implements ErrorController {
+
+    @GetMapping("/error")
+    public String displayErrorPage(Model model, HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        Object message = request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
+
+        if (status != null) {
+            model.addAttribute("errorCode", "Error code: " + status.toString());
+            model.addAttribute("errorMessage", "Error message: " + message.toString());
+        }
+        return "error";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
